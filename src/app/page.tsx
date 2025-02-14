6afd8b657a779c326864b91328b8d7ec09948565
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2025-02-15 04:45:52 +0530

    create Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,5 @@ import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <div className="m-10">
-      <SignedOut>
-        <SignInButton>
-          <Button>Login</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
+  return <div className="m-10">Home Page</div>;
 }
