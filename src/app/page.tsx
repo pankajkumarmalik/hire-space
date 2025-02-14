89ba3e9e06cb27c3d0ed495d283a6fc1b48e1312
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2025-02-14 21:44:43 +0530

    Implement webhook to store users in convex by clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-10">
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <Button>Login</Button>
+        </SignInButton>
       </SignedOut>
 
       <SignedIn>
